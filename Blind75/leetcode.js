391b9269fc7fa1b7c2c4d1ee15fdcb34a9058cce
Alex George <122389120+ag10889@users.noreply.github.com>
2024-10-07 13:24:07 -0400

    Add files via upload

---
+++ Blind75/leetcode.js
@@ -0,0 +1,20 @@
+class calculator {
+
+  
+
+    add(first, second) {
+        return first + second; 
+    } 
+    subtract(first, second) {
+        return first - second;
+    }
+    multiply(first, second) {
+        return first * second;
+    }
+    divide(first, second) {
+        return first/second;
+    }
+    //finna code chatgpt with this calculator 
+    
+}
+
